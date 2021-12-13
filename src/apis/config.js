ec0453d5933f4567010e7dfb48e3258d7b5f5b7d
Daniel <daniel.andres.ramos.v@gmail.com>
2021-12-13 18:18:37 -0300

    second commit

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,9 +1,5 @@
-export const apiUrl = document.location.href.startsWith("http://localhost")
-  ? "http://localhost:5000"
-  : "";
-
-// export const apiUrl = document.location.href.startsWith(
-//   "https://bsale-prueba01.herokuapp.com"
-// )
-//   ? "https://bsale-prueba01.herokuapp.com"
+// export const apiUrl = document.location.href.startsWith("http://localhost")
+//   ? "http://localhost:5000"
 //   : "";
+
+export const apiUrl = "http://209.126.8.246:5007";
